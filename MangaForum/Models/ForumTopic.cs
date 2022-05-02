4b8fe6d629e527a51d597ef798afae44c9374773
GabrieleFirriolo <gabrifirr@gmail.com>
2022-05-02 23:06:58 +0200

    Fix WebApp

---
+++ MangaForum/Models/ForumTopic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MangaForum.Models
+{
+    public class ForumTopic
+    {
+        [Required]
+        public int Name { get; set; }
+        [Required]
+        public Manga Manga { get; set; }
+    }
+}
---
+++ MangaForum/Models/Manga.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MangaForum.Models
+{
+    public class Manga
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Status { get; set; }
+        [Required]
+        public string Image { get; set; }
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public string Synopsis { get; set; }
+
+    }
+}
