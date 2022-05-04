9ab51b6afbb389cd02b9c82c9ab84837027f7a39
GabrieleFirriolo <gabrifirr@gmail.com>
2022-05-04 22:59:55 +0200

    Interfaccia

--- MangaForum/Pages/CreateTopic.cshtml.cs
+++ MangaForum/Pages/CreateTopic.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace MangaForum.Pages
 {
+    [Authorize]
     public class CreateTopicModel : PageModel
     {
         private readonly ILogger<CreateTopicModel> _logger;
